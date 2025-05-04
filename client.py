9bf439bd24f4688e8e3459c5353a4e8a86b079f3
Dhruvil Jayani <dhruviljayani@Mac.attlocal.net>
2025-05-03 23:02:14 -0700

    Increased Message

--- client.py
+++ client.py
@@ -6,19 +6,19 @@ import generated.task_pb2 as task_pb2
 import generated.task_pb2_grpc as task_pb2_grpc
 
 # List of server addresses you want to send tasks to
-# SERVER_ADDRESSES = [
-#     "localhost:50051",
-#     "localhost:50052",
-#     "localhost:50053"
-# ]
+SERVER_ADDRESSES = [
+    "localhost:50051",
+    "localhost:50052",
+    "localhost:50053"
+]
 
-PEERS = {
-    "Server1": "192.168.1.10:50051",  # Machine A
-    "Server2": "192.168.1.10:50052",  # Machine A
-    "Server3": "192.168.1.10:50053",  # Machine A
-    "Server4": "192.168.1.20:50051",  # Machine B
-    "Server5": "192.168.1.20:50052",  # Machine B
-}
+# PEERS = {
+#     "Server1": "192.168.1.10:50051",  # Machine A
+#     "Server2": "192.168.1.10:50052",  # Machine A
+#     "Server3": "192.168.1.10:50053",  # Machine A
+#     "Server4": "192.168.1.20:50051",  # Machine B
+#     "Server5": "192.168.1.20:50052",  # Machine B
+# }
 
 def send_task(server_address, task):
     with grpc.insecure_channel(server_address) as channel:
@@ -39,4 +39,4 @@ def run_task_sender(num_tasks=10, delay=0.5):
         time.sleep(delay)
 
 if __name__ == "__main__":
-    run_task_sender(num_tasks=20, delay=0.3)
+    run_task_sender(num_tasks=100, delay=0.1)
